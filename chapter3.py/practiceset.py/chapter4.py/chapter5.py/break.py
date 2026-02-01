2eff3600101d80b10d333d6a865c62178474ba7f
Akash-paswan <ap6655713@gmail.com>
2026-02-01 12:58:01 +0530

    while_loops.py

---
+++ chapter3.py/practiceset.py/chapter4.py/chapter5.py/break.py
@@ -0,0 +1,38 @@
+#  break keyword in python
+# i = 1 
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i +=1
+    
+# print("end of the loop")   
+
+
+#  continue keywird in   python
+# i = 1 
+# while i<=5:
+#     if(i==3):
+#         i+=1
+#         continue      # skip karta hai aane walw nextr  iteration ko jaise 3 ko kar rh hai 
+#     print(i)
+#     i+=1
+
+
+# print only oddd number fromn 1 to 10 using break keyword 
+# i = 1
+# while i <=10:
+#     if(i%2==0):
+#         i+=1
+#         continue 
+#     print(i)
+#     i+=1
+    
+    #   create even number 
+i = 1
+while i <=10:
+    if(i%2 !=0):
+        i+=1
+        continue 
+    print(i)
+    i+=1
---
+++ chapter3.py/practiceset.py/chapter4.py/chapter5.py/while_loops_practice.py
@@ -0,0 +1,49 @@
+#  practice no. 1 
+#  print number from 1 to 100
+
+# num = 1 
+# while num <=100:
+#     print(num)
+#     num +=1
+
+#   quws no 2 
+# num = 100
+# while num >=1:
+#     print(num)
+#     num -=1
+     
+    #  ques no 3
+# n = 1
+# while n <=10:
+#     print(n*2)
+#     # n +=1
+  
+  
+#    user se input leke multily karo 
+  
+# i = int(input("enter number:")) 
+# n = 1
+# while n <=10:
+#     print(n*i)
+#     n +=1
+ 
+#    ques no 4 
+
+# traverse 
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while idx <len(nums):
+#     print(nums[idx])    # nums[0], num[1],num[2]
+#     idx +=1
+
+#  quws no 5 
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = 36
+i = 0
+while i <len(nums):
+    # print(nums[i])
+    if(nums[i]==x):
+        print("Found at index",i)
+    i+=1
+
+
