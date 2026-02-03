e148a7da7140ab599d34347a10dbb4649d0554fe
Akash-paswan <ap6655713@gmail.com>
2026-02-03 12:42:09 +0530

    using function to for a average of 3 number

---
+++ chapter6/fun_3.py
@@ -0,0 +1,23 @@
+#  average of 3 number  suing function 
+# def  calc_avg(a,b,c):
+#  sum = a+b+c
+#  avg = sum/3
+#  print(avg)
+#  return(avg)
+
+# calc_avg(98,97,99)
+
+
+
+
+
+ # print funct 
+print("apnacollege"  ,end=" ")  # sep = ""       this is a built function
+print("akashpaswan")  # end = "\n"
+
+#  default parameter 
+def cal_prod(a=1,b=2):
+    print(a*b)        # isme na argument pass karna hi default parameters kahte hai 
+    return(a*b)
+
+cal_prod()
