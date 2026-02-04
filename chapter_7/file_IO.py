c638590345964eda8f78894b356f1a6c85138566
Akash-paswan <ap6655713@gmail.com>
2026-02-04 12:06:35 +0530

    input_output.py

---
+++ chapter_7/file_IO.py
@@ -0,0 +1,28 @@
+# f = open("demo.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
+
+#  we can read only 5 lettter 
+# f = open("demo.txt", "r")
+# data = f.read(5)
+# print(data)
+# print(type(data))
+# f.close()
+
+
+#  reding a file in python 
+f= open("demo.txt", "r")
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+
+line3 = f.readline()   # isme 3 line hai to ek empty line ban diya hai output me se 
+print(line3)
+
+
+f.close()
